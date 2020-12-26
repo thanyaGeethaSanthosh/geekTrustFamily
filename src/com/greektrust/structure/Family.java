8ada2d050caaa4e4b8f9fcbe9697b3a3e7430082
thanya <thaniyakc@gmail.com>
2020-12-26 17:01:11 +0530

    added addPartner and addChild in family which work for ideal case

--- src/com/greektrust/structure/Family.java
+++ src/com/greektrust/structure/Family.java
@@ -1,14 +1,31 @@
 package com.greektrust.structure;
 
+import com.greektrust.constants.Gender;
+
+import java.util.Objects;
+
 public class Family {
-    private final Node parent;
+    private final Person parent;
 
     public Family(String parentName) {
-        this.parent = new Node(parentName);
+        this.parent = new Person(parentName);
     }
 
-    public void addChild(String parentName, String childName) {
-        this.parent.addToRightNext(childName);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return Objects.equals(parent, family.parent);
     }
 
+    public void addChild(String motherName, String childName, String gender) {
+        Gender genderOfKid = Gender.valueOf(gender.toUpperCase());
+        Women mother = (Women) this.parent.findPerson(motherName);
+        mother.addChild(childName, genderOfKid);
+    }
+    public void addPartner(String oldMemberName, String partnerName){
+        Person member = this.parent.findPerson(oldMemberName);
+        member.addPartner(partnerName);
+    }
 }
